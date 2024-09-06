6400282579ce7286029f8133512bfb6137320c65
lkimdaryl <78583414+lkimdaryl@users.noreply.github.com>
2024-09-05 19:40:31 -0700

    Revert "added tracking"
    This reverts commit 22856b8e57709c0d4af8c37411dc1ce30843a34e.

--- src/App.tsx
+++ src/App.tsx
@@ -6,23 +6,6 @@ import Main from './components/Main/main'
 function App() {
   ReactGA.initialize('G-BPQDS84M0P');
 
-  ReactGA.send({
-    'hitType': 'pageview', 
-    'title': "Kim's Porfolio",
-    'page': window.location.pathname
-  });
-
-  const handleEvent = (event: { type: string; }) => {
-    ReactGA.event({
-      category: 'user_engagement',
-      action: event.type,
-      label: event.type === 'scroll' ? `${window.scrollY}` : ''
-    });
-  };
-  window.addEventListener('scroll', handleEvent);
-  window.addEventListener('click', handleEvent);
-  window.addEventListener('touchstart', handleEvent);
-
   return (
     <>
       <Header />
