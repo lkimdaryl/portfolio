d8e0f72f0b9824a2e332d791a37bc85b5fd39ba1
lkimdaryl <78583414+lkimdaryl@users.noreply.github.com>
2024-09-05 19:03:53 -0700

    added tracking

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import './App.css'
+import ReactGA from 'react-ga4';
 import Header from './components/Header/header'
 import Hero from './components/Hero/hero'
 import Main from './components/Main/main'
 function App() {
+  ReactGA.initialize('G-BPQDS84M0P');
 
   return (
     <>
