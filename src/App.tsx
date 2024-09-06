ebfae0517024b0fa21d412e6692cf7e253105d46
lkimdaryl <78583414+lkimdaryl@users.noreply.github.com>
2024-09-05 19:51:15 -0700

    revert

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
 import './App.css'
-import ReactGA from 'react-ga4';
 import Header from './components/Header/header'
 import Hero from './components/Hero/hero'
 import Main from './components/Main/main'
 function App() {
-  ReactGA.initialize('G-BPQDS84M0P');
 
   return (
     <>
